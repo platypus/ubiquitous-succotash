1ddf09ed19d02323879307b9cb0553894da98c06
platypus <sibert@wisc.edu>
2021-03-07 12:44:21 -0600

    Update clean_motive.rb

--- ruby/clean_motive.rb
+++ ruby/clean_motive.rb
@@ -2,12 +2,31 @@
 
 require 'csv'
 
+##generates array +/- 1 for point
+##converts array back to csv
+##checks old model for any points in array
+def rounding_error(point, modcsv)
+   rounding = Array.new(27, point)
+   rounding.map!.each_with_index do |el, i|
+      a = el[0].to_i + ((i / 9.5).to_i) - 1
+      b = el[1].to_i + ((i / 3.1).to_i % 3) - 1
+      c = el[2].to_i + (i % 3) - 1
+      el = [a,b,c].join(",")
+   end
+   roundcsv = CSV.parse(rounding.join("\n"), converters: :integer)
+   roundcsv.each do |i|
+     return true if modcsv.include?(i)
+   end
+   return false
+end 
+
+
 if ARGV.length == 5
-	newmod = ARGV[0]
-	oldmod = ARGV[1]
-	motfile = ARGV[2]
-	rotfile = ARGV[3]
-	sumfile = ARGV[4]
+   newmod = ARGV[0]
+   oldmod = ARGV[1]
+   motfile = ARGV[2]
+   rotfile = ARGV[3]
+   sumfile = ARGV[4]
 end
 
 puts motfile
@@ -34,11 +53,13 @@ oldcsv = CSV.read(oldmod, col_sep:" ", converters: :integer, skip_lines: /#/)
 
 keeppts = []
 oldcsv.each do |i|
-	if modcsv.include?(i)
-		keeppts.append(1)
-	else
-		keeppts.append(0)
-	end
+   if modcsv.include?(i)
+      keeppts.append(1)
+   elsif rounding_error(i, modcsv) 
+      keeppts.append(1)
+   else
+      keeppts.append(0)
+   end
 end
 
 motfilein = File.readlines(motfile)
