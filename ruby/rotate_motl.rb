b71df3190fec7c3fea7c20dd7c632d31541d4b2b
platypus <sibert@wisc.edu>
2022-12-09 14:15:07 -0600

    Update rotate_motl.rb

--- ruby/rotate_motl.rb
+++ ruby/rotate_motl.rb
@@ -2,17 +2,19 @@
 
 require 'csv'
 
-if ARGV.length == 1
+if ARGV.length == 2
    file = ARGV[0].chomp
    puts file
+   orange = "#{ARGV[1]},0,0"
+   puts orange
    ARGV.clear
 else
    puts "File name:"
    file = gets.chomp!
 end
 
-puts "Angle (X,Y,Z):"
-angle = gets.split(%r{,\s*}).map(&:to_f)
+#puts "Angle (X,Y,Z):"
+angle = orange.split(%r{,\s*}).map(&:to_f)
 
 header = "CCC,reserved,reserved,pIndex,wedgeWT,NA,NA,NA,NA,NA,xOffset,yOffset,zOffset,NA,NA,reserved,EulerZ(1),EulerZ(3),EulerX(2),reserved,CREATED WITH PEET Version 1.14.0 21-Feb-2020"
 
